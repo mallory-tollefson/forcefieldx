65ec3e52ef66cc9fdd1b44dbd849f7910abb90b2
Mallory Tollefson <mrtollefson@wireless-10-146-243-99.public.utexas.edu>
2019-09-27 16:30:03 -0500

    The extractSecondaryElement function correctly identifies the starting and ending indices of a secondary structure element (helix, sheet, coil) from the user specified secondary structure prediction.

--- modules/potential/src/main/groovy/ffx/potential/groovy/Meld.groovy
+++ modules/potential/src/main/groovy/ffx/potential/groovy/Meld.groovy
@@ -308,11 +308,11 @@ class Energy extends PotentialScript {
      * the correct characters.
      * @return String containing the validated and edited secondary structure restraints.
      */
-    String validateSecondaryStructurePrediction(){
+    String validateSecondaryStructurePrediction() {
         // The only characters that should be present in secondary structure restraint string are 'H' for helix, 'E'
         // for beta sheet and '.' for coil.
-        if (!secondaryStructure.matches("^[HE.]+") && !secondaryStructure.isEmpty()){
-            logger.severe( " Secondary structure restraints may only contain characters 'H', 'E' and '.'")
+        if (!secondaryStructure.matches("^[HE.]+") && !secondaryStructure.isEmpty()) {
+            logger.severe(" Secondary structure restraints may only contain characters 'H', 'E' and '.'")
         }
 
         MolecularAssembly molecularAssembly = assemblyState.getMolecularAssembly()
@@ -320,20 +320,20 @@ class Energy extends PotentialScript {
         int numSecondaryStructure = secondaryStructure.length()
 
         // Only one secondary structure restraint should exist per residue.
-        if(numSecondaryStructure==0){
+        if (numSecondaryStructure == 0) {
             logger.warning(" No secondary structure restraints have been provided. Simulation will proceed " +
                     "with all residues having random coil secondary structure restraints.")
             String randomCoil = StringUtils.leftPad("", numResidues, ".")
             return randomCoil
-        } else if (numSecondaryStructure<numResidues){
+        } else if (numSecondaryStructure < numResidues) {
             logger.warning(" Too few secondary structure restraints exist for number of residues present. " +
                     "Random coil will be added to end residues without provided secondary structure restraints.")
             String extraCoil = StringUtils.rightPad(secondaryStructure, numResidues, '.')
             return extraCoil
-        } else if (numSecondaryStructure==numResidues){
+        } else if (numSecondaryStructure == numResidues) {
             logger.info(" Secondary structure restraints will be added for all residues.")
             return secondaryStructure
-        } else if (numSecondaryStructure>numResidues){
+        } else if (numSecondaryStructure > numResidues) {
             logger.warning(" Too many secondary structure restraints exist for number of residues present."
                     + " Provided secondary structure restraints will be truncated.")
             String truncated = secondaryStructure.substring(0, numResidues)
@@ -350,50 +350,109 @@ class Energy extends PotentialScript {
      * @param distanceForceConstant A float with value supplied by The Dill Group.
      * @param quadraticCut A float with value supplied by The Dill Group. This s
      */
-    void setUpSecondaryMeldRestraints(float torsionForceConstant, float distanceForceConstant, float quadraticCut){
-        torsionForceConstant/=100
-        distanceForceConstant*=100
-        quadraticCut*=10
-        try {
-            ArrayList<ArrayList<Integer>> helices = extractSecondaryElement(secondaryStructure, 'H', 3)
-            System.out.println("Helix element 1: " + helices.get(0))
-            ArrayList<ArrayList<Integer>> sheets = extractSecondaryElement(secondaryStructure, 'E', 3)
-            ArrayList<ArrayList<Integer>> coil = extractSecondaryElement(secondaryStructure, '.', 3)
-        } catch (NullPointerException){
-            logger.warning("Secondary structure element is empty.")
+    void setUpSecondaryMeldRestraints(float torsionForceConstant, float distanceForceConstant, float quadraticCut) {
+        torsionForceConstant /= 100
+        distanceForceConstant *= 100
+        quadraticCut *= 10
+        int minNumResForSecondary = 3
+        String helix = 'H'
+        String sheet = 'E'
+        String coil = '.'
+        Boolean anyHelices
+        Boolean anySheets
+        Boolean anyCoils
+
+        ArrayList<ArrayList<Integer>> helices = extractSecondaryElement(secondaryStructure, helix, minNumResForSecondary)
+        ArrayList<ArrayList<Integer>> sheets = extractSecondaryElement(secondaryStructure, sheet, minNumResForSecondary)
+        ArrayList<ArrayList<Integer>> coils = extractSecondaryElement(secondaryStructure, coil, minNumResForSecondary)
+
+        if(helices.isEmpty()){
+            anyHelices = false
         }
+        if(sheets.isEmpty()){
+            anySheets = false
+        }
+        if(coils.isEmpty()){
+            anyCoils = false
+        }
+
+        // In practice, will need to check anyHelices before popping elements.
+        ArrayList<Integer> firstHelix = helices.pop()
+        logger.info("First Helix Start: " + firstHelix.pop())
+        logger.info("First Helix End: " + firstHelix.pop())
     }
 
     /**
-     * This method determines secondary elements based on the secondary structure restraint predictions. The Dill Group
-     * requires that secondary elements have at least three consecutive residues to be considered a secondary element.
+     * This method determines the starting and ending indices for secondary elements of the requested type based
+     * on the user-supplied secondary structure restraint predictions. The Dill Group requires that secondary
+     * elements have at least three consecutive residues to be considered a secondary element.
      * @param ss A string of the secondary structure prediction.
      * @param elementType Character indicating type of secondary element being searched (helix, coil, sheet).
      * @param minNumResidues Integer minimum of consecutive secondary structure predictions
      * to create a secondary element.
-     * @return ArrayList<ArrayList<Integer>> Contains starting and ending residues for each secondary element.
+     * @return ArrayList<ArrayList<Integer>    > Contains starting and ending residues for each secondary element.
      */
-    ArrayList<ArrayList<Integer>> extractSecondaryElement(String ss, char elementType, int minNumResidues){
-        ArrayList<ArrayList<Integer>> allElements = new ArrayList<ArrayList<Integer>>()
-        int i = 0
-        while(i < ss.length()){
-            if(ss[i] == elementType){
-                int elementStartRes = i;
-                for(int j=i+1; j<ss.length(); j++){
-                  if(!ss[j] == elementType){
-                      int elementLength = j-i;
-                      i = j
-                      if(elementLength>minNumResidues){
-                          int elementEndRes= j-1
-                          ArrayList<Integer> currentElement = new ArrayList<Integer>()
-                          currentElement.add((Integer) elementStartRes)
-                          currentElement.add((Integer) elementEndRes)
-                          allElements.add(currentElement)
-                      }
-                  }
+    ArrayList<ArrayList<Integer>> extractSecondaryElement(String ss, String elementType, int minNumResidues) {
+        ArrayList<ArrayList<Integer>> allElements = new ArrayList<ArrayList<Integer>>() //Will hold starting and ending indices for all found secondary elements of the requested type.
+        int lastMatch = 0 //Track of the most recent index to have a character matching the requested elementType.
+        int i = 0 //Iterates through each index in the secondary structure string.
+        while (i < ss.length()) {
+            if (ss[i].equals(elementType)) {
+                int elementStartIndex = i
+                //Set the starting index for the secondary element as soon as the value at the ith index matches the
+                //requested element type.
+                for (int j = i + 1; j <= ss.length(); j++) {
+                    //Use the jth index to iterate through the secondary structure prediction until the end of the
+                    // secondary element is found.
+                    if (j < ss.length()) {
+                        if (!ss[j].equals(elementType)) {
+                            if (j == lastMatch + 1) {
+                                //If the most recent lastMatch is only one index away, then check and store the
+                                // starting and ending indices of the secondary element.
+                                i = j
+                                //Set i=j so that i begins searching for the next element at the end of the most recent
+                                // secondary element.
+                                //ArrayList<Integer> currentElement = checkLengthAndStoreIndices(j, elementStartIndex, lastMatch, allElements)
+                                int elementLength = j - elementStartIndex
+                                if (elementLength > minNumResidues) {
+                                    //If secondary element is above minimum length, store starting and ending indices
+                                    // of secondary element.
+                                    ArrayList<Integer> currentElement = new ArrayList<Integer>()
+                                    currentElement.add((Integer) elementStartIndex)
+                                    currentElement.add((Integer) lastMatch)
+                                    logger.info("starting index: " + elementStartIndex + "ending index: " + lastMatch)
+                                    allElements.add(currentElement)
+                                }
+                                j = ss.length() + 1
+                                //Since end of current secondary element has been found, exit inner j loop.
+                            }
+                        } else {
+                            lastMatch = j
+                            i++
+                            //If the jth index in the secondary structure string matches the requested element,
+                            // increment j until the end of the secondary element is found.
+                        }
+                    }
+                    if (j == ss.length()) {
+                        //Handle the case when a secondary element is at the very end of the secondary structure string.
+                        i = ss.length() + 1
+                        if (j == lastMatch + 1) {
+                            int elementLength = j - elementStartIndex
+                            if (elementLength > minNumResidues) {
+                                ArrayList<Integer> currentElement = new ArrayList<Integer>()
+                                currentElement.add((Integer) elementStartIndex)
+                                currentElement.add((Integer) lastMatch)
+                                logger.info("starting index: " + elementStartIndex + "ending index: " + lastMatch)
+                                allElements.add(currentElement)
+                            }
+                            j = ss.length() + 1
+                            //Since end of current secondary element has been found, exit inner j loop.
+                        }
+                    }
                 }
-            } else{
+            } else {
                 i++
+                //Increment i until end of secondary structure prediction or until requested secondary element is found.
             }
         }
         return allElements
