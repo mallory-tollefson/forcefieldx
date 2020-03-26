7650d6082cc7930e6a0cdebae235ace12d5654fb
mrkeeney <mallory-tollefson@uiowa.edu>
2020-03-26 15:01:48 -0500

    Edits so that prepareWalkers.sh prints out even columns in walker-by-column format for compatibility with matlab scripts.

--- matlab/prepareWalkers.sh
+++ matlab/prepareWalkers.sh
@@ -14,30 +14,49 @@ file=$1
 numWalkers=$2
 oldWalkers=$3
 
-grep -e "Accept" -e "Reject" ${file} | awk -F " " '{print $1 " " $4}' >> new-walkers.txt
-sed -i "s/\[//g" new-walkers.txt
-sed -i "s/\]//g" new-walkers.txt
-sed -i "s/L=//g" new-walkers.txt
-sed -i "s/,//g" new-walkers.txt
+#grep -e "Accept" -e "Reject" ${file} | awk -F " " '{print $1 " " $4}' >> new-walkers.txt
+#sed -i "s/\[//g" new-walkers.txt
+#sed -i "s/\]//g" new-walkers.txt
+#sed -i "s/L=//g" new-walkers.txt
+#sed -i "s/,//g" new-walkers.txt
 
-if [$oldWalkers]
+if $oldWalkers
 then
+	#Grep the lambda values for each walker into individual files.
 	numWalkers=$((numWalkers-1))
 	for i in $(seq 0 $numWalkers)
 	do
 		grep "${i} " new-walkers.txt | awk -F " " '{print $2}'  >> ${i}.txt
+		lengthArray[$i]=$(wc ${i}.txt | cut -d " " -f 2)
 	done
 
+	#Find the walker that sampled the smallest number of lambdas (aka, slowest walker).
+	min=0
+	for i in ${lengthArray[@]}; do
+    		(( $i < min || min == 0)) && min=$i
+	done
+
+	#Print out the "min" number of lambdas for each walker (all columns have to be 
+	# the same length for the matlab script).
+	for i in $(seq 0 $numWalkers)
+	do
+		tail -n ${min} ${i}.txt >> ${i}-min.txt
+	done 
+
+	#Set up a string to concatenate all of the walkers lambda columns into one file.
 	string=""
 	for i in $(seq 0 $numWalkers)
 	do
-		string="${string} ${i}.txt"
+		string="${string} ${i}-min.txt"
 	done
 
+	#Paste the lambda columns into one file.
 	paste ${string} | column -s $'\t' -t >> old-walkers.txt
 
+	#Clean up generated files.
 	for i in $(seq 0 $numWalkers)
 	do
 		rm ${i}.txt
+		rm ${i}-min.txt
 	done
 fi
